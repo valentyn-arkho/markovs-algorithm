23201e5dbbd49587e44b2610a3d1e0d649b2db3a
Olexij <slmk445@gmail.com>
2018-09-11 20:19:08 +0300

    Changed application interface.

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,21 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
-	
 	public static Main inst;
-	
+
 	public Stage stage;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			inst = this;
-			
+
 			FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource("Main.fxml"));
             Scene scene = new Scene((AnchorPane)loader.load());
-            
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
+
+			scene.getStylesheets().add(getClass().getResource("css/styles.css").toExternalForm());
+
 			stage = primaryStage;
 
 			primaryStage.setScene(scene);
@@ -31,9 +30,7 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
